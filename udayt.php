16322d89664a656075a97e6dacac7066efe33832
UDAYVEERUS <udayveerus348567@gmail.com>
2024-04-30 08:00:07 +0530

    learning php from begining

---
+++ udayt.php
@@ -0,0 +1,10 @@
+<?php
+echo 'hello uday!! <br>'; echo 'Diksha is your best girl friend<br>';
+
+$name = 'sharad';
+$income = '25k';
+
+echo "This guy is $name<br> and his salary is Rs. $income<br>";
+echo "$name is good boy<br>";
+echo "$income is his salary";
+?>
